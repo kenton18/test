96ecd3025974ce0ab94496304b8f0df4571c166c
kenton18 <31765235+kenton18@users.noreply.github.com>
2018-12-11 21:52:05 +0800

    Update test_read.js

--- test/test_read.js
+++ test/test_read.js
@@ -4,7 +4,8 @@ var expect = require('chai').expect;
 var createDate = require('../public/Create_Data.js');
 
 before(function () {
-    createDate("TestingCreate","1997-11-01", "19971-11-01", "1997-11-01", "Meat" );
+    this.enableTimeouts(false)
+    createDate("10","TestingCreate","1997-11-01", "19971-11-01", "1997-11-01", "Meat" );
 });
 
 // Create a food that ready for Read
