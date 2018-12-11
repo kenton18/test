eb82108ae9d3071bc6ef76c4e6914f545db03c09
kenton18 <31765235+kenton18@users.noreply.github.com>
2018-12-12 04:01:06 +0800

    Update test_create.js

--- test/test_create.js
+++ test/test_create.js
@@ -5,8 +5,14 @@ var expect = chai.expect;
 describe('Save records to DB', function() {
     this.timeout(0);
     // Save records
+    
     it('Save foods', async function(){
         const o = await createDate("10","TestingCreate","1997-11-01", "19971-11-01", "1997-11-01", "Meat" );
+        console.log("12")
         expect(o).to.equal('create success');
-})});
+        setTimeout((function() {  
+            return process.kill(process.pid);
+        }), 5000);
+})}
+);
 
